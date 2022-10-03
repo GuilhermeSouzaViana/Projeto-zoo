f25deb2c306e458814c87b26d35909fd6f388bfb
GuiSouzaViana-Ti <vianagui7744@gmail.com>
2022-10-03 17:15:32 -0300

    seventeenth

--- Javascript/form.js
+++ Javascript/form.js
@@ -21,53 +21,62 @@
 
 
     trt.addEventListener("click", function () {
-     if(op.value=="1"){
+      if(op.value=="1"){
       b.style.backgroundImage = "url(../Imagens/joaninha.jpg)"
       f.style.backgroundImage = "url(../Imagens/joani.jpg)"
       t.style.backgroundColor = "#93ac4e"
       f.style.color ="white"
+      op.value="";
         
      }else if(op.value=="2"){
       b.style.backgroundImage = "url(../Imagens/abelha.jpg)"
       f.style.backgroundImage = "url(../Imagens/abelh.jpg)"
       t.style.backgroundColor = "#d59e42"
       f.style.color ="white"
+      op.value="";
      
     }else if(op.value=="3"){
       b.style.backgroundImage = "url(../Imagens/tamandua.jpg)"
       t.style.backgroundColor = "#807753"
       f.style.backgroundImage = "url(../Imagens/tamand.jpg)"
+      op.value="";
      
     }else if(op.value=="4"){
       b.style.backgroundImage = "url(../Imagens/orca.jpg)"
       t.style.backgroundColor = "#4977b4"
       f.style.backgroundImage = "url(../Imagens/orc.jpg)"
+      op.value="";
     
     }else if(op.value=="5"){
       b.style.backgroundImage = "url(../Imagens/polvo.jpg)"
       t.style.backgroundColor = "#1b1006"
       f.style.backgroundImage = "url(../Imagens/polv.jpg)"
       f.style.color ="black"
+      op.value="";
    
     }else if(op.value=="6"){
       b.style.backgroundImage = "url(../Imagens/onça.jpg)"
       t.style.backgroundColor = "#2d2d21"
       f.style.backgroundImage = "url(../Imagens/onç.jpg)"
       f.style.color ="white"
+      op.value="";
     
     }else if(op.value=="7"){
       b.style.backgroundImage = "url(../Imagens/urso.jpg)"
       f.style.backgroundImage = "url(../Imagens/urs.jpg)"
       t.style.backgroundColor = "#f5e0ad"
       f.style.color ="black"
+      op.value="";
     
     }else if(op.value=="8"){
       b.style.backgroundImage = "url(../Imagens/g.jpg)"
       f.style.backgroundImage = "url(../Imagens/gir.jpg)"
       t.style.backgroundColor = "#d1d5de"
       f.style.color ="black"
+      op.value="";
     }else{
       alert("opçao inválida")
+      op.value="";
     }
          
     })
